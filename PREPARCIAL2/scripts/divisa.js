761fb51b6f7299de5d043c62d08bc60f25cac68f
MauroGBecares <maurobecares26@gmail.com>
2023-10-09 14:18:07 -0300

    ActualizacionPreParcial2(Validaciones y css)

--- PREPARCIAL2/scripts/divisa.js
+++ PREPARCIAL2/scripts/divisa.js
@@ -3,12 +3,18 @@ var monedas = []
 function agregarMoneda() {
     let nombre = document.getElementById('nombre-moneda').value
     let costo = document.getElementById('costo-conversion').value
-
+    let mensaje = document.getElementsByClassName('mensaje')
+    
     if(!(validar(nombre, costo))){
         return
     }
 
     localStorage.setItem(nombre, costo)
+    
+    mensaje[0].style.display = "flex"
+    setTimeout(() => {
+        mensaje[0].style.display = "none"
+    }, 3000);
 }
 
 function validar(nombre, costo) {
@@ -20,4 +26,9 @@ function validar(nombre, costo) {
         alert("Debe ingresar el costo de conversion de la moneda y que sea mayor a 0")
         return false
     }
+    if (nombre == "dolar" || nombre == "euro"){
+        alert("Las divisas dolar y euro ya estan agregadas")
+        return false
+    }
+    return true
 }
--- PREPARCIAL2/scripts/index.js
+++ PREPARCIAL2/scripts/index.js
@@ -1,24 +1,16 @@
-var monedas = [{ nombre: "dolar", valor: 900 }, { nombre: "euro", valor: 500 }]
+const dolar = 900
+const euro = 500
 
 
 function cargarDivisas() {
-    let desplegable = document.getElementById('select-moneda')
-    for (let i = 0; i < monedas.length; i++) {
-        if (monedas[i].nombre != localStorage.key(i)) {
-            monedas.push({ nombre: localStorage.key(i), valor: parseFloat(localStorage.getItem(localStorage.key(i))) })
-        }
-    }
-    console.log(monedas)
+    let desplegable = document.getElementById("select-moneda");
     if (localStorage.length > 0) {
         for (let i = 0; i < localStorage.length; i++) {
-            monedas.forEach(moneda => {
-                if (localStorage.key(i) == moneda.nombre) {
-                    let option = document.createElement('option')
-                    option.value = moneda.nombre
-                    option.textContent = moneda.nombre.charAt(0).toUpperCase() + moneda.nombre.slice(1).toLowerCase()
-                    desplegable.appendChild(option)
-                }
-            });
+            let option = document.createElement('option')
+            let moneda = localStorage.key(i)
+            option.value = moneda
+            option.textContent = moneda.charAt(0).toUpperCase() + moneda.slice(1).toLowerCase()
+            desplegable.appendChild(option)
         }
     }
 }
@@ -27,26 +19,32 @@ function calcularDivisa() {
     let monto = document.getElementById('txt-cantidad').value
     let divisa = document.getElementById('select-moneda').value
     let parrafo = document.getElementById('resultado')
-    
-    if (!(validar(monto, divisa))){
+
+    if (!(validar(monto, divisa))) {
         return
     }
 
-    monedas.forEach(moneda => {
-        if (divisa == moneda.nombre) {
-            parrafo.innerHTML = `La cotización del ${divisa.charAt(0).toUpperCase() + divisa.slice(1).toLowerCase()} es: ${monto * moneda.valor}`
-            return
-        }
-    });
+    switch (divisa) {
+        case "dolar":
+            parrafo.innerHTML = `La cotización del ${divisa.charAt(0).toUpperCase() + divisa.slice(1).toLowerCase()} es: ${monto * dolar}`
+            break;
+        case "euro":
+            parrafo.innerHTML = `La cotización del ${divisa.charAt(0).toUpperCase() + divisa.slice(1).toLowerCase()} es: ${monto * euro}`
+            break;
+        default:
+            parrafo.innerHTML = `La cotización del ${divisa.charAt(0).toUpperCase() + divisa.slice(1).toLowerCase()} es: ${monto * localStorage.getItem(divisa)}`
+            break;
+    }
 }
 
-function validar(monto, divisa){
-    if (monto <= 0){
+function validar(monto, divisa) {
+    if (monto <= 0) {
         alert("Debe ingresar un número mayor que 0")
         return false
     }
-    if (divisa == ""){
+    if (divisa == "") {
         alert("Debe ingresar una divisa para convertir")
         return false
     }
+    return true
 }
